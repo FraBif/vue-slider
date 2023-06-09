af2ad699966ee012c7964a6d05544ffec3cfd34c
FraBif <francescobif@hotmail.it>
2023-06-09 16:07:00 +0200

    removed v-for, adjusted script and exercise done

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const { createApp } = Vue;
 createApp({
     data() {
         return {
-             images = [
+             images : [
                 {
                     image: 'img/01.webp',
                     title: 'Marvel\'s Spiderman Miles Morales',
@@ -25,7 +25,7 @@ createApp({
                     title: "Marvel's Avengers",
                     text: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.',
                 }
-            ]
+            ] ,
             
             activeIndex : 0,
         }
